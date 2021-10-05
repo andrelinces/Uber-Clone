fd13cb130d3ffe6dc65ea961b354ecc154ea8636
Andre Linces <andrelinces@yahoo.com.br>
2021-10-04 21:36:38 -0300

    Classes create for to views, to enter the app and register the app.

---
+++ Uber Clone/View Controllers/CadastroViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CadastroViewController.swift
+//  Uber Clone
+//
+//  Created by Andre Linces on 04/10/21.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+}
---
+++ Uber Clone/View Controllers/EntrarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EntrarViewController.swift
+//  Uber Clone
+//
+//  Created by Andre Linces on 04/10/21.
+//
+
+import UIKit
+
+class EntrarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+}
