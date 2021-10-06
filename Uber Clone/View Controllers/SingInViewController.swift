35b7f2e1ff5f75879924a2aeee50a706001b0fef
Andre Linces <andrelinces@yahoo.com.br>
2021-10-06 19:57:47 -0300

    Creating main view after the user logs into the app.

--- Uber Clone/View Controllers/SingInViewController.swift
+++ Uber Clone/View Controllers/SingInViewController.swift
@@ -6,8 +6,68 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SingInViewController: UIViewController {
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var passaword: UITextField!
+    
+    @IBAction func buttonSingin(_ sender: Any) {
+        
+        let buttonReturn = self.validateFields()
+        if buttonReturn == " "{
+            
+            //authenticate the user (Login)
+            let authentication = Auth.auth()
+            
+            if let emailRecovered = self.email.text {
+                
+                if let passawordRecovered = self.passaword.text {
+                    
+                    authentication.signIn(withEmail: emailRecovered, password: passawordRecovered) { (user, error) in
+                        
+                        if error == nil {
+                            
+                            if user != nil {
+                                
+                                print("user successful authentication!")
+                            }
+                            
+                        }else{
+                            
+                            print("Error login user!")
+                        }
+                        
+                    }
+                }
+            }
+            
+            
+        }else{
+            
+            print("The field \(buttonReturn) was not filled in !")
+        }
+        
+    }
+    
+    //Method for validate user-entered fields
+    func validateFields() -> String {
+        
+        if (self.email.text?.isEmpty)! {
+            
+            return "E-mail"
+            
+        }else if (self.passaword.text?.isEmpty)! {
+            
+            return "Passaword"
+            
+        }
+        
+       return " "
+    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
