42dc6d1d7070d157feb4f923e07e07cbfe30be25
Andre Linces <andrelinces@yahoo.com.br>
2021-10-06 21:03:59 -0300

    Inserting user logout button.

--- Uber Clone/View Controllers/ViewController.swift
+++ Uber Clone/View Controllers/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //retriveing the objetc
+        let authentication = Auth.auth()
+        
+        //singOut user
+        
+        do {
+            try try authentication.signOut()
+        } catch  {
+            print("Error singOut user!!")
+        }
+        
+        authentication.addStateDidChangeListener { authentication, user in
+            //If the logged in user has a value, there is a logged in user.
+            if let userLogged = user {
+                
+                //passing user to main view.
+                self.performSegue(withIdentifier: "segueLoginMain", sender: nil)
+                
+            }
+            
+        }
+        
+        
+        
     }
     //Hiding navigation bar
     override func viewWillAppear(_ animated: Bool) {
