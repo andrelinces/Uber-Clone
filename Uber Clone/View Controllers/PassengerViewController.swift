73b0a822ac7c334255e525b827b33ecfec6e2b8e
Andre Linces <andrelinces@yahoo.com.br>
2021-10-07 11:17:11 -0300

    Displays the user's location on the map.

--- Uber Clone/View Controllers/PassengerViewController.swift
+++ Uber Clone/View Controllers/PassengerViewController.swift
@@ -16,18 +16,48 @@ class PassengerViewController: UIViewController, CLLocationManagerDelegate {
     //instantiating object CllocationManager
     var locationManager = CLLocationManager()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //own class that will manage the localization features
+        locationManager.delegate = self
+        //Defining the best location accuracy
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //Request user location authorization
+        locationManager.requestWhenInUseAuthorization()
+        //Updating user location
+        locationManager.startUpdatingLocation()
+        
+    }
+    //Method for updating user location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //Retrieving the coordinates
+        if let coordinateRecovered = manager.location?.coordinate {
+            
+            //Defines user's location with the coordinateRecovered object and with a distance accuracy 200
+            let region = MKCoordinateRegion.init(center: coordinateRecovered, latitudinalMeters: 200, longitudinalMeters: 200)
+            
+            //using the mapview of the app
+            mapView.setRegion(region, animated: true)
+            
+            //Create an annotation for the user's location
+            let userAnnotation = MKPointAnnotation()
+            userAnnotation.coordinate = coordinateRecovered
+            userAnnotation.title = "Your location !!"
+            mapView.addAnnotation(userAnnotation)
+        }
+          
+    }
     
-    
-    let region = MKCoordinateRegion(center: <#T##CLLocationCoordinate2D#>, span: <#T##MKCoordinateSpan#>)
-    let mapView = MKMapView.setRegion(self.region)
+    //let region = MKCoordinateRegion(center: <#T##CLLocationCoordinate2D#>, span: <#T##MKCoordinateSpan#>)
+    //let mapView = MKMapView.setRegion(self.region)
     
     override func viewDidAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    
     //Logout user
     @IBAction func logoutUser(_ sender: Any) {
         
