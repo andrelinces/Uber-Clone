9561d1bf6f5449c37eb43b5174b56ca6c6197d79
Andre Linces <andrelinces@yahoo.com.br>
2021-10-18 19:28:19 -0300

    Creating folders:
    *Global Helpers
     - Global Protocol
     - Global Extension
     - Global Methods
    
    Creating Creating folders:
    
    *Global Helpers
     - Global Protocol
     - Global Extension
     - Global Methods
    
    Creating Protocol Archive.

---
+++ Uber Clone/GlobalHelpers/GlobalProtocol/GlobalProtocol.swift
@@ -0,0 +1,30 @@
+//
+//  GlobalProtocol.swift
+//  Uber Clone
+//
+//  Created by Andre Linces on 18/10/21.
+//
+
+import UIKit
+
+//Protocolo tem a função de definir qual método será usado. Para o model funcionar, células modelo da tableView
+protocol tableViewCompatible {
+    
+    var reuseIdentifier : String {get}
+
+    func celForTableView ( tableView: UITableView, atIndexPath indePath: IndexPath ) -> UITableViewCell
+    
+}
+
+//Protocolo tem a função de definir qual método será usado. Para o model funcionar, células modelo do collectionView.
+protocol collectionViewCompatible {
+    
+    var reuseIdentifier :  String {get}
+    
+    func cellForCollectionView ( collectionView: UICollectionView, atindexPath indePath: IndexPath ) -> UICollectionView
+    
+    
+    
+    }
+    
+
